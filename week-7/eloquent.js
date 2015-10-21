032b2042d258e4f74003977f0e896b66a180cef8
Steven Broderick <brodericksteven@gmail.com>
2015-10-20 17:22:26 -0700

    Copied JS from replit

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -7,17 +7,45 @@
 var foo = 5;
 foo++;
 
+// Favorite Food
+var favFood = prompt("What's your favorite food?")
+console.log("Hey! That's my favorite too!")
+
 
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
 
+// Chess Board
+
+for (row = 0; row < 8; row++) {
+    if (row % 2 === 0) {
+        console.log(" # # # #");
+    }
+    else {
+        console.log("# # # # ");
+    }
+}
 
 
 // Functions
 
 // Complete the `minimum` exercise.
-
+function min(x, y) {
+    if (x < y) {
+        return x;
+    }
+    else {
+        return y;
+    }
+}
 
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+me = {
+    name: "Steven",
+    age: 29,
+    favFoods: ["shabu shabu", "sushi", "spaghetti"],
+    quirk: "bad tree-climber"
+};
