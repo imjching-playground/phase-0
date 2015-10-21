bd5adfcc1d2f10a8efbbe61321fe1c79a4909bd2
Steven Broderick <brodericksteven@gmail.com>
2015-10-20 22:47:23 -0700

    Finish 7.8 release 2 (group challenge)

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,38 @@
+/*
+
+SUM ___________________________________________________________________________
+
+DEFINE a function called "sum()" that takes one parameter "integer_array" (an
+array of integers)
+  ASSIGN a local variable "total" and set it equal to 0
+  (FOR...IN... loop:)
+  FOR (EACH) index IN "integer_array"
+    ADD the value at that index in "integer_array" to "total"
+  RETURN "total"
+END
+
+
+MEAN___________________________________________________________________________
+
+DEFINE a function called "mean()" that takes one parameter "integer_array"
+  ASSIGN a local variable "total" and set it equal to the result of calling
+  function "sum()" on "integer_array"
+  RETURN the result of dividing "total" by the length of "integer_array"
+END
+
+
+MEDIAN_________________________________________________________________________
+
+DEFINE a function called "median()" that takes one parameter "integer_array"
+  IF length of "integer_array" is divisible by two (i.e. even)
+    ASSIGN a local variable "middle_two" and set it equal to empty array
+    DIVIDE length of "integer_array" by 2 and push that value to "middle_two"
+    DIVIDE length of "integer_array" by 2, then subtract 1, and push that value
+    to "middle_two"
+    RETURN the result of calling function "mean()" on "middle_two"
+  ELSE (i.e. odd)
+    ASSIGN local variable "middle_index" equal to length of "integer_array"
+    minus 1, then divided by two
+    RETURN value in "integer_array" at index "middle_index"
+
+*/
