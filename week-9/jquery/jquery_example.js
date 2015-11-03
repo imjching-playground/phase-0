254935a691bcb69674161eb1af17270f111a46f7
Steven Broderick <brodericksteven@gmail.com>
2015-11-03 15:26:50 -0800

    Complete 9.3.2 pairing

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -13,19 +13,42 @@ $(document).ready(function(){
 
   //Link this script and the jQuery library to the jQuery_example.html file and analyze what this code does.
 
-$('body').css({'background-color': 'pink'})
+$('body').css({'background-color': 'pink'});
 
 //RELEASE 2:
   //Add code here to select elements of the DOM
-
+mascot = $('.mascot');
+h1 = $('h1');
+img = $('img');
 
 //RELEASE 3:
   // Add code here to modify the css and html of DOM elements
+firstH1 = $('body>h1');
+firstH1.css('color', 'green');
+firstH1.css('border', 'black solid 2px');
+firstH1.css('visibility', 'visible');
 
 
 //RELEASE 4: Event Listener
   // Add the code for the event listener here
+$('img').on('mouseover', function(e){
+  e.preventDefault();
+  $(this).attr('src', 'copperheads.png');
+  $(this).css('border', 'black solid 1px');
 
+
+  $(this).animate({
+    borderWidth: '4px',
+    height: '486',
+    width: '820'
+  }, 1000
+  );
+});
+
+$('img').on('mouseleave', function(e){
+  e.preventDefault();
+  $(this).attr('src', 'dbc_logo.png');
+});
 
 //RELEASE 5: Experiment on your own
 
