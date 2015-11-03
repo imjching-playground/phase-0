aebcb414604bcfebaf549f682b4be0c73eab5da4
Steven Broderick <brodericksteven@gmail.com>
2015-11-03 15:40:07 -0800

    Finish reflection

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -25,7 +25,7 @@ img = $('img');
   // Add code here to modify the css and html of DOM elements
 firstH1 = $('body>h1');
 firstH1.css('color', 'green');
-firstH1.css('border', 'black solid 2px');
+firstH1.css('border', '2px solid black');
 firstH1.css('visibility', 'visible');
 
 
@@ -34,9 +34,7 @@ firstH1.css('visibility', 'visible');
 $('img').on('mouseover', function(e){
   e.preventDefault();
   $(this).attr('src', 'copperheads.png');
-  $(this).css('border', 'black solid 1px');
-
-
+  $(this).css('border', '1px solid green');
   $(this).animate({
     borderWidth: '4px',
     height: '486',
@@ -52,9 +50,27 @@ $('img').on('mouseleave', function(e){
 
 //RELEASE 5: Experiment on your own
 
+// we nested the animations inside of the mouseover function above (release 4)
 
 
 
 
 
-})  // end of the document.ready function: do not remove or write DOM manipulation below this.
+});  // end of the document.ready function: do not remove or write DOM manipulation below this.
+
+
+
+/* REFLECTION
+What is jQuery?
+It's a JavaScript library that makes it easier to manipulate the DOM. It
+particularly excels at modifying styling of of DOM elements, animations, and
+event-handling.
+
+What does jQuery do for you?
+It reduces the amount of code that I have to write to manipulate the DOM and
+allows me to create more interactivity on a site without spending as much time
+as I would with pure JS.
+
+What did you learn about the DOM while working on this challenge?
+You can't select images by filename (seems obvious why).
+*/
